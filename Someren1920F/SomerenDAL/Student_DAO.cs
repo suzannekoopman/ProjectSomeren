4efe5cec5c14c52ed97b0c5d2cb70c4a71db263c
suzannekoopman <47417257+suzannekoopman@users.noreply.github.com>
2019-09-30 16:40:05 +0200

    Opdracht1

---
+++ Someren1920F/SomerenDAL/Student_DAO.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+using SomerenModel;
+
+namespace SomerenDAL
+{
+    public class Student_DAO : Base
+    {
+      
+        public List<Student> Db_Get_All_Students()
+        {
+            string query = "SELECT Id, Name FROM Students";
+            SqlParameter[] sqlParameters = new SqlParameter[0];
+            return ReadTables(ExecuteSelectQuery(query, sqlParameters));
+        }
+
+        private List<Student> ReadTables(DataTable dataTable)
+        {
+            List<Student> students = new List<Student>();
+
+            foreach (DataRow dr in dataTable.Rows)
+            {
+                Student student = new Student()
+                {
+                    Number = (int)dr["Id"],
+                    Name = (String)(dr["Name"].ToString())
+                };
+                students.Add(student);
+            }
+            return students;
+        }
+
+    }
+}
